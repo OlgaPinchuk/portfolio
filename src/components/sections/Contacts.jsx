8b67b7c8d75e2fb3ccc035c0483cb7e89e5500fe
Olga Pinchuk <pinchukolya@gmail.com>
2021-09-17 16:35:28 +0200

    refactoring

--- src/components/sections/Contacts.jsx
+++ src/components/sections/Contacts.jsx
@@ -6,7 +6,7 @@ import { contacts } from "../../data/pageSections.json";
 export default function Contacts() {
   const { title, description } = contacts;
   return (
-    <section className="page-section contact" id="contact">
+    <section className="page-section contact" id="contacts">
       <div className="container">
         <h2 className="section-title">{title}</h2>
         <div className="descriptions">
